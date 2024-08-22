e1896f54c9bf7ba1953f9d6b27fab9b94e48c6e1
Adham Khaled <105177075+adhamkhaled312@users.noreply.github.com>
2024-08-22 17:30:36 +0300

    Time: 23 ms (67.9%), Space: 94.9 MB (49.16%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,30 +3,36 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1=nums1.size();
         int n2=nums2.size();
-        int n3=n1+n2;
-        vector<int>merged(n3);
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i < n1 && j < n2){
-            if(nums1[i] <= nums2[j]){
-                merged[k++] = nums1[i++];
+        if(n1 > n2) return findMedianSortedArrays(nums2,nums1);
+        int left=0;
+        int right=n1;
+        int total=(n1+n2+1)/2;
+        while(left <= right){
+            int r1=INT_MAX;
+            int r2=INT_MAX;
+            int l1=INT_MIN;
+            int l2=INT_MIN;
+            int mid1=left+(right-left)/2;
+            int mid2=total-mid1;
+            if(mid1 < n1 ) r1=nums1[mid1];
+            if(mid2 < n2 ) r2=nums2[mid2];
+            if(mid1-1 >= 0) l1=nums1[mid1-1];
+            if(mid2-1 >= 0) l2=nums2[mid2-1];
+            if(l1 <= r2 && l2 <= r1 ){
+                if((n1+n2)%2==0){
+                    return (max(l1,l2) + min(r1,r2))/2.0;
+                }
+                else{
+                    return max(l1,l2);
+                }
+            }
+            else if(l1 < r2){
+                left=mid1+1;
             }
             else{
-                merged[k++]=nums2[j++];
+                right=mid1-1;
             }
         }
-        while(i < n1){
-            merged[k++] = nums1[i++];
-        }    
-        while(j < n2){
-            merged[k++] = nums2[j++];
-        }
-        if(n3 % 2 == 0){
-            return ((double (merged[n3/2])+merged[n3/2 -1])/2);
-        }
-        else{
-            return merged[n3/2];
-        } 
+        return 0;
     }
 };

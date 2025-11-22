ddd62c0b300725d21d52c957e71d70f181f5d8a9
Adham Khaled <105177075+adhamkhaled312@users.noreply.github.com>
2025-11-22 10:17:26 +0200

    Time: 55 ms (5.06%), Space: 8.6 MB (50.59%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,8 +3,8 @@ public:
     int mySqrt(int x) {
         if(x==0 || x==1) return x;
         for(int i=1; i <= x; i++){
-            if(i * i == x) return i;
-            else if(i * i > x) return i-1;
+            if(i == x/i) return i;
+            else if(i > x/i) return i-1;
         }
         return 0;
     }

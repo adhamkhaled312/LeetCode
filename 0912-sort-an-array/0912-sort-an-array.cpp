f2d96c906fc742c5d87f2b259b4411d6733977e9
Adham Khaled <105177075+adhamkhaled312@users.noreply.github.com>
2025-11-24 12:11:53 +0200

    Time: 1537 ms (6.27%), Space: 70.9 MB (83.62%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,43 +1,25 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums,0,nums.size()-1);
+        quickSort(nums,0,nums.size()-1);
         return nums;
     }
     
-    void mergeSort(vector<int>&arr, int l, int r) {
-        if (l < r) {
-            int mid = l + (r - l) / 2;
-
-            mergeSort(arr, l, mid);
-            mergeSort(arr, mid + 1, r);
-
-            merge(arr,l,mid,r);
+    void quickSort(vector<int>&arr, int l, int r) {
+        if(l>r) return;
+        int mid = l+(r-l)/2;
+        int pivot = arr[mid];
+        swap(arr[mid],arr[r]);
+        int pi=l;
+        for(int i=l; i<r; i++){
+            if(arr[i] < pivot){
+            swap(arr[i],arr[pi]);
+            pi++;
+            }
         }
+        swap(arr[pi],arr[r]);
+        quickSort(arr,l,pi-1);
+        quickSort(arr,pi+1,r);
     }
 
-    void merge(vector<int>&arr, int l, int m, int r) {
-        const int size1 = m - l + 1;
-        int size2 = r - m;
-        vector<int>leftArr(size1);
-        vector<int>rightArr(size2);
-
-        for (int i = 0; i < size1; i++) {
-            leftArr[i] = arr[l + i];
-        }
-        for (int i = 0; i < size2; i++) {
-            rightArr[i] = arr[m + 1 + i];
-        }
-
-        int i = 0;
-        int j = 0;
-        int k = l;
-        while (i < size1 && j < size2) {
-            if (leftArr[i] < rightArr[j]) arr[k++] = leftArr[i++];
-            else arr[k++] = rightArr[j++];
-        }
-
-        while (i < size1) arr[k++] = leftArr[i++];
-        while (j < size2) arr[k++] = rightArr[j++];
-    }
 };

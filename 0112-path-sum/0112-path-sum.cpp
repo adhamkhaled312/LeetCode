776d04d9ae7054157d54a370541d2861936938dc
Adham Khaled <105177075+adhamkhaled312@users.noreply.github.com>
2026-01-29 14:24:29 +0200

    Time: 0 ms (100%), Space: 21.5 MB (66.29%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if (!root) return false;
-
-        stack<pair<TreeNode*, int>> dfs;
-
-        dfs.push({root, root->val});
-
-        while (!dfs.empty()) {
-            auto [curr, sum] = dfs.top();
-            dfs.pop();
-
-            if (!curr->left && !curr->right && sum == targetSum)
-                return true;
-
-            if (curr->right)
-                dfs.push({curr->right, sum + curr->right->val});
-
-            if (curr->left)
-                dfs.push({curr->left, sum + curr->left->val});
-        }
-
-        return false;
+        if(!root) return false;
+        if(!root->left && !root->right)
+            return targetSum-root->val == 0;
+        targetSum-=root->val;
+        return hasPathSum(root->left,targetSum) || hasPathSum(root->right,targetSum);
     }
 };
